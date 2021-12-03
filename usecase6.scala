03be21fe7adcbe9be70b033fad91449bb36a3161
shabatasleemtaj1 <shabatasleemtaj1@gmail.com>
2021-12-03 23:43:07 +0530

    totalhours

---
+++ usecase6.scala
@@ -0,0 +1,29 @@
+object Employee {
+  def main(args: Array[String]): Unit = {
+    val Emp_type = scala.util.Random.nextBoolean()
+    var attendancecheck = scala.util.Random.nextBoolean()
+    val Wageperhour = 20
+    var Days = 1
+    var maxhours = 0
+    while (Days <= 20 && maxhours <= 100) {
+      attendancecheck match {
+        case true =>
+          Emp_type match
+          {
+            case true =>
+              var hours = 8
+              var Emp_type = "fulltime"
+            case false =>
+              var hours = 4
+              var Emp_type = "parttime"
+              maxhours = maxhours+hours
+              var totalwage=maxhours*Wageperhour
+              println("total hour is:" +maxhours)
+              println("totalwage is:"+ totalwage)
+          }
+        case _ => println("employee absent")
+      }
+      Days= Days+1
+    }
+  }
+}
